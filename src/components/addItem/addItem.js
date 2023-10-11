cca92e2bf3c72a24e252870f3039ea744432379e
Salah Najem <salah.najem123412345@gmail.com>
2023-10-11 22:27:11 +0300

    Added quantity column

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,38 +1,74 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class addItem extends Component {
-    state = {
-        product: '',
-        price: ''
-    }
+  state = {
+    product: "",
+    price: "",
+    quantity: 1,
+  };
 
-    handleChange = (e) => {
-        console.log(e.target.id + ": " + e.target.value)
-        this.setState({
-            [e.target.id]: e.target.value
-        })
-    }
+  handleChange = (e) => {
+    console.log(e.target.id + ": " + e.target.value);
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        this.props.add(this.state)
-        this.setState({
-            product: '',
-            price: ''
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.add(this.state);
+    this.setState({
+      product: "",
+      price: "",
+      quantity: 1,
+    });
+  };
 
-    render(){
-        return(
-            <div className="item">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
-                </form>
-            </div>
-        )
-    }
+  decreaseQuantity = (e) => {
+    e.preventDefault();
+    if (this.state.quantity > 1)
+      this.setState({ quantity: --this.state.quantity });
+  };
+  increaseQuantity = (e) => {
+    e.preventDefault();
+    if (this.state.quantity < 100)
+      this.setState({ quantity: ++this.state.quantity });
+  };
+
+  render() {
+    return (
+      <div className="item">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.product}
+            placeholder="Enter Product"
+            id="product"
+            onChange={this.handleChange}
+            required
+          />
+          <input
+            type="number"
+            value={this.state.price}
+            placeholder="Enter Price"
+            id="price"
+            onChange={this.handleChange}
+            required
+          />
+          <div className="quantity-container" onChange={this.handleChange}>
+            <button onClick={this.decreaseQuantity}>-</button>
+            <input
+              type="number"
+              id="quantity"
+              value={this.state.quantity}
+            ></input>
+            <button onClick={this.increaseQuantity}>+</button>
+          </div>
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default addItem;
